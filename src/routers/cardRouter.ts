33ee7c78f2e56bc0be4c525dc97e72b995e21f63
mltlima <miguellt@dcc.ufrj.br>
2022-07-10 02:40:50 -0300

    validatecard schema

--- src/routers/cardRouter.ts
+++ src/routers/cardRouter.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import schemas from '../schemas/index.js';
-
 import * as cardController from '../controllers/cardController.js';
 import validateApiKey from '../middlewares/validateApiKeyMiddleware.js';
+import { validateSchema } from '../middlewares/schemasMiddleware.js';
 
 const cardRouter = Router();
 //todo: schema
-cardRouter.post("/card", validateApiKey, cardController.createCard);
+cardRouter.post("/card", validateApiKey, validateSchema(schemas.createCardSchema), cardController.createCard);
 
 export default cardRouter;
